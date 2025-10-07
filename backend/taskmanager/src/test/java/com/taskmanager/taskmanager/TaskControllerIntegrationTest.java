5199f880b59bed54e0d9aa382afa02a92d8cdfa3
IT21302862 <sanayasamadhi@gmail.com>
2025-10-07 11:39:47 +0530

    BE0003: integrate H2 database for testing

--- backend/taskmanager/src/test/java/com/taskmanager/taskmanager/TaskControllerIntegrationTest.java
+++ backend/taskmanager/src/test/java/com/taskmanager/taskmanager/TaskControllerIntegrationTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import org.junit.jupiter.api.Test;
@@ -18,6 +19,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 public class TaskControllerIntegrationTest {
 
